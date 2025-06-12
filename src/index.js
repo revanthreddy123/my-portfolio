39359d8bc86cfe95634b25d485b19ab7adc75e17
revanthreddy123 <ar.revanthreddy@gmail.com>
2025-06-12 20:25:21 +0530

    12345

--- src/index.js
+++ src/index.js
@@ -5,3 +5,6 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
+if (window.innerWidth < 768) {
+  document.body.classList.add('no-anim');
+}
